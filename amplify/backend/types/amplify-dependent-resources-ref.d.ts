2b5fe0b0b366532fc262a340f6541712eb411959
Richard Banh <richardbanh@macwookiebookie.lan>
2022-07-18 21:07:57 -0700

    working on adding auth backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "medtrain5d2c6186": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -5,66 +5,73 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Image } from 'react-bootstrap';
 
+import steth from './Asset/Stethascope.png';
 const App = () => {
 	return (
 		<div className='App'>
 			<Navbar bg='light' expand='lg'>
 				<Container>
-					<Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+					<Navbar.Brand href='#home'>MedTrain</Navbar.Brand>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='me-auto'>
-							<Nav.Link href='#home'>Home</Nav.Link>
-							<Nav.Link href='#link'>Link</Nav.Link>
-							<NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-								<NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-								<NavDropdown.Item href='#action/3.2'>
-									Another action
-								</NavDropdown.Item>
-								<NavDropdown.Item href='#action/3.3'>
-									Something
-								</NavDropdown.Item>
-								<NavDropdown.Divider />
-								<NavDropdown.Item href='#action/3.4'>
-									Separated link
-								</NavDropdown.Item>
+							<Nav.Link href='#action/3.1'>Search for Clinic</Nav.Link>
+							<Nav.Link href='#action/3.2'>Sign up</Nav.Link>
+							<Nav.Link href='#action/3.2'>Sign in</Nav.Link>
+							<NavDropdown title='Legalise' id='basic-nav-dropdown'>
+								<Nav.Link href='#action/3.4'>Terms and Conditions</Nav.Link>
+								<Nav.Link href='#action/3.4'>Privacy</Nav.Link>
 							</NavDropdown>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
 			</Navbar>
-			<div className='px-4 pt-5 my-5 text-center border-bottom'>
-				<h1 className='display-4 fw-bold'>Medical Train</h1>
-				<div className='col-lg-6 mx-auto'>
-					<p className='lead mb-4'>
-						Quickly find an avaliable walk-in clinic, With appointments or
-						current wait times
-					</p>
-					<div className='d-grid gap-2 d-sm-flex justify-content-sm-center mb-5'>
-						<button
-							type='button'
-							className='btn btn-primary btn-lg px-4 me-sm-3'
-						>
-							Sign Up Your Clinic
-						</button>
-						<button
-							type='button'
-							className='btn btn-outline-secondary btn-lg px-4'
-						>
-							Find Near by Clinic
-						</button>
+			<div
+				className='px-4 pt-5 d-flex align-items-center justify-content-center'
+				style={{
+					backgroundImage: `url(${steth})`,
+					minHeight: '60vh',
+					backgroundSize: '1650px 584px',
+					backgroundRepeat: 'none',
+				}}
+			>
+				<div style={{ minWidth: '100%', marginTop: '-10%' }}>
+					<h1 className='display-4 fw-bold'>Medical Train</h1>
+					<div className='col-lg-6 mx-auto'>
+						<p className='lead mb-4'>
+							Quickly find an avaliable walk-in clinic, With appointments or
+							current wait times
+						</p>
+						<div className='d-grid gap-2 d-sm-flex justify-content-sm-center mb-5'>
+							<button type='button' className='btn btn-success btn-lg px-4'>
+								Find Nearby Clinic
+							</button>
+							<button
+								type='button'
+								className='btn btn-primary btn-lg px-4 me-sm-3'
+							>
+								Sign Up Your Clinic
+							</button>
+						</div>
 					</div>
-				</div>
-				<div className='overflow-hidden'>
-					<div className='container px-5'></div>
+					<div className='overflow-hidden'>
+						<div className='container px-5'></div>
+					</div>
 				</div>
 			</div>
 			<Container>
-				<Image></Image>
-				<Image></Image>
-				<Image></Image>
+				<h1 className='display-4 fw-bold' style={{ marginTop: '20px' }}>
+					Find Nearest Clinics
+				</h1>
+				<input
+					type='text'
+					class='form-control'
+					placeholder='Search your home address'
+					aria-label='Search your home address'
+					aria-describedby='basic-addon2'
+					style={{ marginTop: '30px' }}
+				></input>
 			</Container>
 		</div>
 	);
