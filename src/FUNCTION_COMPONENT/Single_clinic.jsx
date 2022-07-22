fafc7e88d3ad94ac5798b98973053fd5d0a67821
Richard Banh <richardbanh@macwookiebookie.lan>
2022-07-21 22:06:07 -0700

    working on info expansion

--- src/FUNCTION_COMPONENT/Single_clinic.jsx
+++ src/FUNCTION_COMPONENT/Single_clinic.jsx
@@ -1,5 +1,15 @@
+import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import { useState } from 'react';
+import down from '../Asset/down.png';
+import up from '../Asset/up.png';
 const Single_clinic = () => {
+	const [expand_bool, set_expand_bool] = useState(false);
+
+	const handle_expand = () => {
+		set_expand_bool(!expand_bool);
+	};
+
 	return (
 		<Container>
 			<div
@@ -61,72 +71,26 @@ const Single_clinic = () => {
 						</a>
 					</div>
 				</div>
+				<div
+					className='hours_expand'
+					onClick={() => {
+						handle_expand();
+					}}
+				>
+					Hours
+					{expand_bool ? (
+						<>
+							<img className='up_arrow' src={up} />
+						</>
+					) : (
+						<>
+							<img className='down_arrow' src={down} />
+						</>
+					)}
+				</div>
 			</div>
 		</Container>
 	);
 };
 
 export default Single_clinic;
-
-<Container>
-	<div
-		style={{
-			borderStyle: 'solid',
-			borderWidth: '1px',
-			borderRadius: '3px',
-			margin: '1%',
-			minHeight: '10vh',
-			padding: '10px',
-			marginBottom: '50px',
-		}}
-		className='d-flex align-items-center justify-content-center flex-column'
-	>
-		<div
-			className='d-flex align-items-start justify-content-around'
-			style={{ width: '100%', height: '100%' }}
-		>
-			<p className='text_size'>Walkin Clinic, Joe Bob</p>
-			<div>
-				<p className='text_size'>Wait time</p>
-				<p className='text_size color_green'>2 Hr</p>
-			</div>
-			<div>
-				<p className='text_size'>Appointments avaliable</p>
-				<p className='text_size color_green'>10</p>
-			</div>
-
-			<div>
-				<p className='text_size'>Distance from you</p>
-				<p className='text_size color_green'>100 KM</p>
-			</div>
-
-			<div className='d-flex flex-column'>
-				<a
-					className='text_size link'
-					href={`https://www.google.com/maps/place/${
-						'6129' +
-						'+' +
-						'River' +
-						'+' +
-						'Rd' +
-						',' +
-						'+' +
-						'Port' +
-						'+' +
-						'Alberni' +
-						',' +
-						'+' +
-						'BC'
-					}`}
-					target='_blank'
-					rel='noreferrer'
-				>
-					6129 River Rd, Port Alberni, BC
-				</a>
-				<a className='text_size link' href={`tel:+${7789527551}`}>
-					7789527551
-				</a>
-			</div>
-		</div>
-	</div>
-</Container>;
