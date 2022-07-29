095efdd925fdce7aa254e1e7548b5cddb6af4d29
Richard Banh <richardbanh@macwookiebookie.lan>
2022-07-28 20:32:22 -0700

    backend done, i think, need to do testing

---
+++ dumbie_data.js
@@ -0,0 +1,67 @@
+const dumbie = [
+	{
+ "uuid": "dfljkalksjiiwie2e23",
+ "address": "123 Main St, Toronto, ON, M2M2M2",
+ "appointment_available": "10",
+ "clinic_name": "Bon John Clinic",
+ "email_address": "bonjon@clinic.ca",
+ "last_update": "45min",
+ "phone_number": "+1-416-555-5555",
+ "city":"toronto",
+ "times": {
+  "fri": {
+   "end_hr": "06",
+   "end_min": "04",
+   "open": "true",
+   "start_hr": "23",
+   "start_min": "03"
+  },
+  "mon": {
+   "end_hr": "06",
+   "end_min": "04",
+   "open": "true",
+   "start_hr": "23",
+   "start_min": "03"
+  },
+  "sat": {
+   "end_hr": "06",
+   "end_min": "04",
+   "open": "false",
+   "start_hr": "23",
+   "start_min": "03"
+  },
+  "sun": {
+   "end_hr": "06",
+   "end_min": "04",
+   "open": "false",
+   "start_hr": "23",
+   "start_min": "03"
+  },
+  "thur": {
+   "end_hr": "06",
+   "end_min": "04",
+   "open": "true",
+   "start_hr": "23",
+   "start_min": "03"
+  },
+  "tues": {
+   "end_hr": "06",
+   "end_min": "04",
+   "open": "true",
+   "start_hr": "23",
+   "start_min": "03"
+  },
+  "wed": {
+   "end_hr": "06",
+   "end_min": "04",
+   "open": "true",
+   "start_hr": "23",
+   "start_min": "03"
+  }
+ },
+ "username": "vonjon",
+ "waittime": "45min"
+}
+];
+
+// "city":"vancouver"
